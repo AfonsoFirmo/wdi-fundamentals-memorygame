a412b1234f7bc7949f6d5090a5c52091aa315210
afonsofirmo <afonsofirmo@gmail.com>
2019-08-29 20:33:54 +1000

    worked on js file

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,23 @@
 
-var cardOne= "queen";
-var cardTwo="queen";
+
+
+const cards = ["queen","queen","king","king"];
+const cardsInPlay =[];
+
+
+var cardOne= cards[0];
+
+cardsInPlay.push(cardOne);
+
+console.log("User flipped a " + cardsInPlay);
+
+var cardTwo= cards[2];
+
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardsInPlay[1]);
+
+if (cardsInPlay.length === 2) {
+	console.log("User flipped two cards")
+}
 var cardThree="king";
 var cardFour="king";
-console.log("User flipped a " + cardOne + " and a " + cardTwo);
