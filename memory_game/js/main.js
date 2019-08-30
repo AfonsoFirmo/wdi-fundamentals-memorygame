bbfd63f60cd23103ed702e564aac872e4c7c756f
afonsofirmo <afonsofirmo@gmail.com>
2019-08-30 22:16:36 +1000

    added js flipped card logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -4,20 +4,34 @@
 const cards = ["queen","queen","king","king"];
 const cardsInPlay =[];
 
-
+var message="";
 var cardOne= cards[0];
 
 cardsInPlay.push(cardOne);
 
-console.log("User flipped a " + cardsInPlay);
+console.log("User flipped a " + cardsInPlay[0]);
 
 var cardTwo= cards[2];
 
 cardsInPlay.push(cardTwo);
 console.log("User flipped " + cardsInPlay[1]);
 
+/*if (hour < 18) {
+  greeting = "Good day";
+} else {
+  greeting = "Good evening";
+}*/
+
 if (cardsInPlay.length === 2) {
-	console.log("User flipped two cards")
+	console.log('you flipped two cards');
 }
+
+if (cardOne !== cardTwo){
+	console.log('you found a match');
+	} else {
+		alert('sorry try again');
+	}
+	
+console.log(message);
 var cardThree="king";
 var cardFour="king";
