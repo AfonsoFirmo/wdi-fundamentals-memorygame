a254ab2b3202c4b457122d4d87daa061878dea0e
afonsofirmo <afonsofirmo@gmail.com>
2019-09-05 12:17:14 +1000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,20 +1,45 @@
 
 
 
-const cards = ["queen","queen","king","king"];
+const myArray = [
+	{
+	rank: 'king',
+	suit: 'hearts',
+	cardImage: "images/king-of-hearts.png"
+	},
+	{
+	rank: 'king',
+	suit: 'diamonds',
+	cardImage: "images/king-of-diamonds.png"
+	},
+	{
+	rank: 'queen',
+	suit: 'hearts',
+	cardImage: "images/queen-of-hearts.png"
+	},
+	{
+	rank: 'queen',
+	suit: 'diamonds',
+	cardImage:"images/queen-of-diamonds.png"
+	}
+	];
+
 const cardsInPlay =[];
 
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-  console.log("You found a match!");
+  alert("Was it a match? Yes, you found a match!");
 	} else {
-  console.log("Sorry, try again.");
+  alert("Was it a match? Nop, try again.")
+  console.log("next card is..");
 	}
 }
 
 function flipCard(cardId) {
-	console.log("User flipped a " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped a " + myArray[cardId].rank);
+	cardsInPlay.push(myArray[cardId].rank);
+	console.log(myArray[cardId].cardImage);
+	console.log(myArray[cardId].suit);
 	checkForMatch();
 	/*if (cardsInPlay.length === 2) {
 		console.log('you flipped two cards');
